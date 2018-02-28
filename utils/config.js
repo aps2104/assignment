39aca834a98e366f93436488c350fab7fd470a3f
Apurva <apurva8904@gmail.com>
2018-02-28 23:39:20 +0530

    added moment js

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,12 @@
 export const  defaultData = [{
     id: '8xf0y6ziyjabvozdd253nd',
-    timestamp: 1467166872634,
+    timestamp: 1519841213132,
     title: 'This is a dummy comment',
     author: 'Ryan',
     voteScore: 6
   },{
     id: '6ni6ok3ym7mf1p33lnez',
-    timestamp: 1468479767190,
+    timestamp: 1519841213188,
     title: 'This is also a dummy comment',
     author: 'Tom',
     voteScore: -5
--- utils/format.js
+++ utils/format.js
@@ -1,10 +1,12 @@
+import moment from 'moment'
+
 export const timestampToTime = (timestamp) => {
-  return formatTime(new Date(timestamp))
+  return formatTime(moment(timestamp))
 }
 
 function formatTime(date) {
-    var hour = date.getHours(),
-        minute = date.getMinutes()
-
-    return  hour + " hours " + minute + " minutes";
+    const now = moment();
+    const hourAgo = now.diff(date, 'hour');
+    const minuteAgo = now.diff(date, 'minute');
+    return  hourAgo ? hourAgo + " hours " + minuteAgo + " minutes ago" : minuteAgo + " minutes ago";
 }
